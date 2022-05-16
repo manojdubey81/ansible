180051e42ee8e5491b9b3c4038a62a27209d4f70
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-05-16 14:54:16 -0700

    Updated ec2 script file

--- create-ec2.sh
+++ create-ec2.sh
@@ -23,10 +23,20 @@ PRIVATE_IP=$(aws ec2 describe-instances \
              --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${COMPONENT}" \
              --output text | awk '{print$2}')
 
+PUBLIC_IP=$(aws ec2 describe-instances \
+             --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" \
+             --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${COMPONENT}" \
+             --output text | awk '{print$2}')
+
+INST_NAME=$(aws ec2 describe-instances \
+             --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}" \
+             --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${COMPONENT}" \
+             --output text | awk '{print$1}')
+
 if [ ! -z "${PRIVATE_IP}" ]; then
     echo  "  "
     echo -e "\e[33mInstance ${COMPONENT} is already exists, Please check your AWS Account!!\e[0m"
-    echo -e "\e[33mInstance ID = ${PRIVATE_IP}\e[0m"
+    echo -e "\e[33mName Tag = ${INST_NAME}, PublicIP = ${PUBLIC_IP}, PrivateIp = ${PRIVATE_IP}\e[0m"
     echo -e "----------------------------------------------------\n"
     exit 3
 else
