75c4eb0db8dea713d006e862c01ca3fc14743482
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-05-17 16:34:13 -0700

    Updated ec2 script file

--- create-pvt-hzone.sh
+++ create-pvt-hzone.sh
@@ -8,8 +8,7 @@ fi
 COMPONENT=$1
 
 VPC_ID=$(aws ec2 describe-vpcs | jq '.Vpcs[].VpcId' | sed -e 's/"//g')
-PVT_HZ=$(aws route53 list-hosted-zones-by-name \
-        | jq '.HostedZone.Id' | sed -e 's/"//g' | sed -e 's/\/hostedzone\// /')
+PVT_HZ=$(aws route53 list-hosted-zones-by-name | jq '.HostedZone.Id' | sed -e 's/"//g' | sed -e 's/\/hostedzone\// /')
 
 
 #PVT_HOST_ZONE=$PVT_HZ.HostedZone.Id.replace("/hostedzone/","")
