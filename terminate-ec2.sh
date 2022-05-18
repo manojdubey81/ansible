0039593b59088317e6e61da6a59b9d0a18dddee8
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-05-18 11:42:20 -0700

    Updated ec2 script file

--- terminate-ec2.sh
+++ terminate-ec2.sh
@@ -1,5 +1,11 @@
 #!bin/bash
 
+# This Script will perform below activities:
+# 1. Terminate all application's instance
+# 2. Delete "A" Record Type from Private Hosted Zone (roboshop.internal)
+# 3. Delete Private Hosted Zone (roboshop.internal)
+
+
 if [ -z "$1" ]; then
   echo -e '\e[32mPlease Select Component Name to terminate!!\e[0m'
   exit 1
@@ -8,10 +14,15 @@ fi
 COMPONENT="$1"
 
 VPC_ID=$(aws ec2 describe-vpcs | jq '.Vpcs[].VpcId' | sed -e 's/"//g')
+
+echo -e "\e[32mVPC ID = ${VPC_ID}\e[0m"
+echo -e "----------------------------------------------------\n"
+
 PVT_HOST_ZONE=$(aws route53 list-hosted-zones-by-name \
                 --dns-name roboshop.internal | jq '.HostedZones[].Id' | sed -e 's/"//g' | sed -e 's/\/hostedzone\// /')
 
-echo "Private Hosted Zone ID= " $PVT_HOST_ZONE
+echo -e "\e[32mPrivate Hosted Zone ID = $PVT_HOST_ZONE\e[0m"
+echo -e "----------------------------------------------------\n"
 
 terminate_instance() {
 
@@ -20,11 +31,11 @@ terminate_instance() {
            --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${COMPONENT}" \
            --output text | awk '{print$2}')
 
-#  if [ ! -z "${PVT_ID}" ]; then
+  if [ -z "${PVT_ID}" ]; then
      sed -e "s/IPADDRESS/${PVT_IP}/" -e "s/COMPONENT/${COMPONENT}-dev/" -e "s/ACTION/DELETE/" route53.json >/tmp/record.json
      aws route53 change-resource-record-sets --hosted-zone-id ${PVT_HOST_ZONE} --change-batch file:///tmp/record.json | jq
 
-#  fi
+  fi
 
   INST_ID=$(aws ec2 describe-instances \
                  --filters "Name=tag:Name,Values=${COMPONENT}" \
@@ -50,3 +61,5 @@ else
   echo -e "\e[31m${COMPONENT} termination started:-\e[0m"
   terminate_instance
 fi
+
+aws route53 delete-hosted-zone --id ${PVT_HOST_ZONE}
