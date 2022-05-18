238d33caad6a5c3f1fff06343d6bda4fbae2c7c4
Manoj Dubey <manoj.dubey81@yahoo.in>
2022-05-18 12:06:13 -0700

    Updated ec2 script

--- display-ec2.sh
+++ display-ec2.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+# This Script will Display all running application instance details
+# Also update the Inventory file to use in ansible playbook with instance details
 
 if [ -z "$1" ]; then
   echo -e '\e[32mPlease Select Component Name to display the details!!\e[0m'
@@ -29,27 +31,30 @@ display_instance() {
 
 
   if [ ! -z "${PUBLIC_IP}" ]; then
-          echo  "  "
-          echo -e "\e[33mName = ${INST_NAME}\e[0m, \e[32mPublicIP = ${PUBLIC_IP}\e[0m, \e[33mPrivateIp = ${PRIVATE_IP}\e[0m, \e[35mStatus = 'running'\e[0m"
-          INST_NAME_CAPS=$(echo ${INST_NAME} | tr [a-z] [A-Z])
-          echo [${INST_NAME_CAPS}] >> inv
-          echo ${PRIVATE_IP} >> inv
+    echo  "  "
+    echo -e "\e[33mName = ${INST_NAME}\e[0m, \e[32mPublicIP = ${PUBLIC_IP}\e[0m, \e[33mPrivateIp = ${PRIVATE_IP}\e[0m, \e[35mStatus = 'running'\e[0m"
+
+    INST_NAME_CAPS=$(echo ${INST_NAME} | tr [a-z] [A-Z])
+
+    echo [${INST_NAME_CAPS}] >> inv
+    echo ${PRIVATE_IP} >> inv
+
   else
-          echo -e "\e[32m${COMPONENT} application is not running at this time\e[0m"
+    echo -e "\e[32m${COMPONENT} application is not running at this time\e[0m"
   fi
 }
 
 if [ "$1" == "all" ]; then
-  echo  "  "
-  echo -e "\e[31mAll application's status are below:-\e[0m"
-  echo  "  "
-  for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch ; do
-    COMPONENT=$component
-    display_instance
-  done
-  echo -e "---------------------------------------------------------------------------------\n"
+    echo  "  "
+    echo -e "\e[31mAll application's status are below:-\e[0m"
+    echo  "  "
+    for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch ; do
+        COMPONENT=$component
+        display_instance
+    done
+    echo -e "---------------------------------------------------------------------------------\n"
 else
-  echo  "  "
-  echo -e "\e[31m${COMPONENT} running instance is with below details:-\e[0m"
-  display_instance
+    echo  "  "
+    echo -e "\e[31m${COMPONENT} running instance is with below details:-\e[0m"
+    display_instance
 fi
